ceaaef041bf58647726fffe3a5f5cbec9ebcf31a
Eric Masseran <rico.masseran@gmail.com>
2018-02-09 12:07:08 +0100

    Add upload page
    - Add from for uploading
    - Accept images up to 1MB
    - Save them in instance/uploads/ where the app is launched

--- image-uploader/imageuploader.py
+++ image-uploader/imageuploader.py
@@ -1,12 +1,52 @@
-from flask import Flask, url_for, render_template
+import os
+from flask import Flask, url_for, render_template, redirect, \
+                request, send_from_directory, flash
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = 'uploads/'
+ALLOWED_EXTENSIONS = set(['png', 'jpg'])
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
 
 
 @app.route("/")
 def images():
     return render_template('layout.html')
 
-@app.route("/upload")
+@app.route("/upload", methods=['GET', 'POST'])
 def upload():
-    return render_template('layout.html')
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            save_file(file, filename)
+            return redirect(url_for('uploaded_file', filename=filename))
+    return render_template('upload.html')
+
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    print(app.instance_path)
+    return send_from_directory(
+            os.path.join(app.instance_path,
+                            app.config['UPLOAD_FOLDER']), filename)
+
+
+def save_file(file, filename):
+    upload_path = os.path.join(app.instance_path,
+                    app.config['UPLOAD_FOLDER'])
+    if not os.path.exists(upload_path):
+        os.makedirs(upload_path)
+    file.save(os.path.join(upload_path, filename))
+
+def allowed_file(filename):
+    return '.' in filename and \
+            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
